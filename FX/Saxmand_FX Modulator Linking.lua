33576aa5e2c0da1932ae2e58aafb10e263726ab9
saxmand <saxmand@gmail.com>
2025-04-22 11:30:26 -0600

    test

--- FX/Saxmand_FX Modulator Linking.lua
+++ FX/Saxmand_FX Modulator Linking.lua
@@ -1,7 +1,8 @@
 -- @description FX Modulator Linking
 -- @author Saxmand
--- @version 0.1.7
+-- @version 0.1.8
 -- @provides
+--   [effect] FX/FX Modulator Linking/*.jsfx
 --   Helpers/*.lua
 -- @changelog
 --   + Initial version
