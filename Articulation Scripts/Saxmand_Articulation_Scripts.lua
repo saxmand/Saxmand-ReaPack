5215d2e2aaaa381ec859e9000c63fc12593310db
saxmand <saxmand@gmail.com>
2026-02-04 17:37:40 +0100

    AS 0.3.3

--- Articulation Scripts/Saxmand_Articulation_Scripts.lua
+++ Articulation Scripts/Saxmand_Articulation_Scripts.lua
@@ -1,7 +1,7 @@
 -- @description Articulation Scripts
 -- @author Saxmand
 -- @package Articulation Scripts
--- @version 0.1.8
+-- @version 0.1.9
 -- @about
 --   A complete system for the MOST advanced articulation scripts
 -- @provides
