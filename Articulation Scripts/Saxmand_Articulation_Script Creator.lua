9c27f0e676e3de2c041fc94e776ca2e628c7c325
saxmand <saxmand@gmail.com>
2026-02-04 16:28:34 +0100

    As 0.1.9

--- Articulation Scripts/Saxmand_Articulation_Script Creator.lua
+++ Articulation Scripts/Saxmand_Articulation_Script Creator.lua
@@ -1,7 +1,7 @@
 -- @description Articulation Script Creator
 -- @author Saxmand
 -- @package Articulation Scripts
--- @version 0.0.8
+-- @version 0.0.9
 -- @about
 --   Create new articulation scripts
 -- @provides
