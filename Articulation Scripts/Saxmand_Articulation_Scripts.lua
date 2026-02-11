e0c95b116d085aabdd04b8afa546e99dfd3fc655
saxmand <saxmand@gmail.com>
2026-02-11 18:47:08 +0100

    AS 0.7.6

--- Articulation Scripts/Saxmand_Articulation_Scripts.lua
+++ Articulation Scripts/Saxmand_Articulation_Scripts.lua
@@ -1,7 +1,7 @@
 -- @description Articulation Scripts
 -- @author Saxmand
 -- @package Articulation Scripts
--- @version 0.5.4
+-- @version 0.5.5
 -- @about
 --   A complete system for the MOST advanced articulation scripts
 -- @provides
